ad4416916f8b60652644a6c488bfb78b3d5d5a24
jukkanghost2 <80653155+jukkanghost2@users.noreply.github.com>
2021-11-17 10:58:20 -0300

    algunos tests

---
+++ sim_1/new/extensor_tb.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/16/2021 04:40:02 PM
+// Design Name: 
+// Module Name: extensor_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module extensor_tb;
+ parameter DATA_WIDTH = 32;
+ parameter IMM_WIDTH = 16;
+
+	//INPUTS
+  reg     [DATA_WIDTH - 1:0]    i_instruccion;
+  	//OUTPUTS
+  wire   [DATA_WIDTH - 1:0]    o_branchoffset;
+
+  EXTENSOR extensorcito (
+    .i_instruccion      (i_instruccion), 
+    .o_branchoffset     (o_branchoffset)
+  );
+    
+    initial begin
+        i_instruccion = 32'b00000000000000001001001001000000;
+        #20
+        i_instruccion = 32'b00000000000000000001001001000000;
+        #20
+        $finish;
+    end
+endmodule
---
+++ sim_1/new/mux_tb.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/11/2021 04:53:07 PM
+// Design Name: 
+// Module Name: mux_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux_tb;
+  parameter DATA_WIDTH = 32;
+
+	//INPUTS
+  reg     [DATA_WIDTH - 1:0]    i_a;
+  reg     [DATA_WIDTH - 1:0]    i_b;
+  reg     [DATA_WIDTH - 1:0]    i_c;
+  reg     [DATA_WIDTH - 1:0]    i_d;
+  reg     [1:0]                      i_select;
+  	//OUTPUTS
+  wire   [DATA_WIDTH - 1:0]    o_o;
+
+  MUX4 muxito4 (
+    .i_a      (i_a), 
+    .i_b      (i_b), 
+    .i_c      (i_c), 
+    .i_d      (i_d), 
+    .i_select     (i_select), 
+    .o_o     (o_o)
+  );
+
+    initial begin
+        i_a = 0;
+        i_b = 0;
+        i_c = 0;
+        i_d = 0;
+        i_select = 2'b00;
+        #20
+        i_a = 3;
+        i_b = 4;
+        i_c = 5;
+        i_d = 6;
+        #20
+        i_select = 2'b01;
+        #20
+        i_select = 2'b10;
+        #20
+        i_select = 2'b11;
+        #20
+        $finish;
+    end
+    
+endmodule
